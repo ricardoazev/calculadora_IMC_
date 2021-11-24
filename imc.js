b69702242c66127303e9db80b1ccc67a9d6a5e5f
ricardoazev <ricardometronome@gmail.com>
2021-11-24 14:22:33 -0300

    primeiro commit

---
+++ imc.js
@@ -0,0 +1,37 @@
+
+const calcular = document.getElementById("calcular");
+
+
+function imc (){
+    const nome = document.getElementById("nome").value;
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado = document.getElementById("resultado");
+
+    if(nome.value !== '' && altura !== '' && peso !== ''){ //se nome,altura,peso for diferente de vazio
+        const valorIMC =(peso / (altura * altura)).toFixed(1);// tofixed numero de casas decimais
+
+        let classificacao = '';
+
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25){
+            classificacao = 'com peso ideal. Parabéns!!!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = ' com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = ' com obesidade grau II.'
+        }else{
+            classificacao = 'obesidade grau III. Cuidado!!!'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+    }else{
+        resultado.textContent = 'Preencha todos os campos!!!'
+    }
+
+}
+
+calcular.addEventListener('click', imc);//escutar o evento quando alguen clicar em calcular
